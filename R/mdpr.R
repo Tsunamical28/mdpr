720bf2f33c6d1cef5f9b672e1e755ad55a6e48ae
David Ashear <dcashear@gmail.com>
2015-05-12 15:22:12 -0400

    package descrip

---
+++ R/mdpr.R
@@ -0,0 +1,13 @@
+#' mdpr: Municipal Data and Pricing
+#' 
+#' mdpr will be used by traders of fixed income instruments, mainly municipal and US    
+#' government bonds. Functionality will include pricing (clean and dirty price, yield to
+#' worst, and assorted other functions), risk analysis (dv01, convexity, etc.), and relative
+#' value metrics.
+#'
+#'
+#' To learn more about mdpr, ask Wong.
+#'
+#' @docType package
+#' @name mdpr
+NULL
