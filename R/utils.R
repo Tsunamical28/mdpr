9bb3c9c0b42e21fc8ddfec12b8237c81164431f2
Tsunamical28 <mr.lawrencewong@gmail.com>
2015-05-13 11:36:31 -0400

    Added comments for Util function and ammended safe_ifelse

--- R/utils.R
+++ R/utils.R
@@ -1,5 +1,15 @@
 # Utility functions -------------------------------------------------------
 
+#' Coalesce to Return First Non-NA
+#' 
+#' Evaluates the given argument in the order in which they were passed.
+#' This function will return the first argument that does NOT evaluate to NA.
+#' 
+#' @param ... Arguments to evaluate
+#' @return  The first non NA argument 
+#' @examples
+#' coalesce(NA, "second","third",4, NA,"sixth")
+#' @export
 coalesce <- function(...){
   Reduce(function(x,y) {
     i <- which(is.na(x))
@@ -9,12 +19,43 @@ coalesce <- function(...){
 }
 
 
-
-safe_ifelse <- function(cond, yes, no){  
-  if(cond){
-    yes 
-  } else no
+#' Vectorized \code{ifelse} with Class Retention
+#' 
+#' The normal behavior for the \code{ifelse} function is to return the resulting vector
+#' with a class type coerced to be the same as the \code{yes} argument. However, when 
+#' a Date type vector is passed as an argument, Dates lose their class when the 
+#' condition is evaluated. The resulting vector is of type numeric. 
+#' This function \code{safe_ifelse} restores the normal behavior for Date vectors.
+#' 
+#' @param cond the test to determine if yes/no
+#' @param yes the value or operation to return if \code{cond} evaluates to \code{TRUE}
+#' @param no the value or operation to return if \code{cond} evaluates to \code{FALSE} 
+#' @return the resulting vector with retention of the \code{yes} argument class type
+#' @examples
+#' d = as.Date(c('2015-01-01','2015-08-08'))
+#' safe_ifelse(d=='2015-01-01',d-10,d)
+#' @export
+safe_ifelse <- function(cond, yes, no){
+  structure(ifelse(cond, yes, no), class = class(yes))
 }
 
+
+#' Vectorized function for rounding to multiple of any number
+#' 
+#' Vectorized version of \code{plyr::round_any}, which is a function that rounds 
+#' to multiple of any number. 
+#' 
+#' @param x vector of numbers to round
+#' @param accuracy vector of multiples to round the vector \code{x}
+#' @return vector with \code{x} rounded to the nearest \code{accuracy}
+#' @examples
+#' round_any_vec(c(52,199,14),10)
 #' @export
-round_any_vec <- Vectorize(round_any, c("x"))
+round_any_vec <- Vectorize(plyr::round_any, c("x","accuracy"))
+
+
+
+
+
+
+
