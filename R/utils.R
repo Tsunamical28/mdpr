c6c0ead42fe74c2471808b0449d5df469b5da17c
David Ashear <dcashear@gmail.com>
2015-05-14 13:57:34 -0400

    utils for merge

--- R/utils.R
+++ R/utils.R
@@ -40,7 +40,10 @@ coalesce <- function(...){
 safe_ifelse <- function(cond, yes, no, class_to_use = class(no)){
   structure(ifelse(cond, yes, no), class = class_to_use)
 }
-
+# safe_ifelse <- function(cond, yes, no){  
+#   if(cond) yes 
+#   else no
+# }
 
 #' Vectorized function for rounding to multiple of any number
 #' 
