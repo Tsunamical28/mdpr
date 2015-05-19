1572cb7077c62500b8a66755d5445781f21151d3
David Ashear <dcashear@gmail.com>
2015-05-19 13:46:49 -0400

    added close_date and eod functions

--- R/dependent_constants.R
+++ R/dependent_constants.R
@@ -1,15 +1,48 @@
 # Constants Built after Utils ---------------------------------------------
 
+#' Get Previous Close Date
+#' 
+#' Uses the Architect DB to get the business date before
+#' the argument's business date.
+#' 
+#' @param date The date to use
+#' @return  The previous business date using the SIFMA Calednar
+#' @examples
+#' get_close_date()
+#' @export
+get_close_date <- function(date = Sys.Date()){  
+    as.Date(dbQuery(paste0(
+      "SELECT CalendarDate
+            FROM [Architect].[dbo].[CAL_BusDay]
+            WHERE NextBusinessDate = '", date, "' AND
+            CalendarType = 2")))
+}
+
+
+#' Get End of Day
+#' 
+#' Used to get a date with hours and minutes for the last
+#' second of the day. This guarantees that all trades on a given
+#' date will be captured.
+#' 
+#' @param date The date to use
+#' @return  POSIX variable for the date with hours, minutes, 
+#' and seconds equal to 23:59:59
+#' @examples
+#' get_eod()
+#' @export
+get_eod <- function(date = Sys.time()){    
+  lubridate::hour(date) <- 23;
+  lubridate::minute(date) <- 59;
+  lubridate::second(date) <- 59
+  date
+}
+
 # Default dates based on date that package is attached
-c_close_date <- Sys.Date()
-c_close_date <- as.Date(dbQuery(paste0(
-        "SELECT CalendarDate
-        FROM [Architect].[dbo].[CAL_BusDay]
-        WHERE NextBusinessDate = '", c_close_date, "'")))
+c_close_date <- get_close_date()
 assign("c_close_date", c_close_date, mdpr_globals)
-c_as_of_datetime <- Sys.time()
-lubridate::hour(c_as_of_datetime) <- 23;
-lubridate::minute(c_as_of_datetime) <- 59;
-lubridate::second(c_as_of_datetime) <- 59
+
+c_as_of_datetime <- get_eod()
 assign("c_as_of_datetime", c_as_of_datetime, mdpr_globals)
 attach(mdpr_globals)
+
